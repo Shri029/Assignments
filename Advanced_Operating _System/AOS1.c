148ae6dc7f3dd25c839b29b6c07ff05fce78a093
Shivani Tiwary <30630384+Shri029@users.noreply.github.com>
2018-04-25 01:31:39 +0530

    AOS1.c

--- Advanced_Operating _System/AOS1.c
+++ Advanced_Operating _System/AOS1.c
@@ -1,10 +1,14 @@
 //ADVANCED OPERATING SYSTEM
-//ASSIGNMENT 1 
-
+//PROBLEM STATEMENT
 
 /*
 
-Program              : Implementation for Visiting current directory, it's subdirectories and printing file's information
+Write a program (using appropriate Unix / Linux system calls) that visits every directory, starting
+with the current directory. It should be able to handle loops in the directory hierarchy.
+The program should read the directory and print the following information for all files in the
+directory – owner, file types, access permissions, and access times.
+
+
 Author               : © Shivani Tiwary-36, Surajit Das-41
 Created on           : March 22, 2018 09:20 IST
 Language             : C
